78fa84293eb4464accd250abf8f1fa53855e8ba0
Jared Frazier <cscidev001@gmail.com>
2023-09-22 17:55:39 +0200

    export  func

--- src/PlasmaCellDiff.jl
+++ src/PlasmaCellDiff.jl
@@ -4,7 +4,7 @@ using DrWatson
 
 export germinal_center_ode_params
 export germinal_center_exit_pathway, germinal_center_exit_pathway_jacobian
-export BCR, CD40
+export BCR, CD40, stochastic_regulatory_signal
 
 include("martinez_germinal_center_model.jl")
 
