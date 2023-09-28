38588aa196066eac08a645f472773086d4ec9c60
Jared Frazier <cscidev001@gmail.com>
2023-09-28 10:12:36 +0200

    exporting bcr_subnetwork and nullcline funcs

--- src/PlasmaCellDiff.jl
+++ src/PlasmaCellDiff.jl
@@ -1,10 +1,11 @@
 module PlasmaCellDiff
 
 export GerminalCenterODEParams
-export germinal_center_exit_pathway
+export germinal_center_exit_pathway, bcr_subnetwork
 export germinal_center_exit_pathway_jacobian
 export BCR, CD40, gaussian_regulatory_signal
 export NORMAL_DISTRIBUTION_PEAK_Y_STD_5, NORMAL_DISTRIBUTION_PEAK_Y_STD_1
+export blimp1_nullcline, bcl6_nullcline
 
 include("martinez_germinal_center_model.jl")
 include("nullclines.jl")
