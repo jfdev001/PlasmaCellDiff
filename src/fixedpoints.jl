03ec1e60f2aa46d531f9233fa7ee45c501bcb881
Jared Frazier <cscidev001@gmail.com>
2023-10-05 00:08:46 +0200

    possible strategy for fp calculation

---
+++ src/fixedpoints.jl
@@ -0,0 +1,5 @@
+# Could compute fixed points at desired timesteps in the the interval
+# because the `ChaosTools.fixedpoints` function sets t = 0.0, but one 
+# could easily pass a timestep parameter to this and see fixed points at that
+# points (look at Figure 3 in color and notice how fixed points correspond
+# to different points on the gaussian curve)
